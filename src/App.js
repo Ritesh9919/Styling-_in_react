8ea221b16865214817a492ada8b250f646a88daf
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-10-20 11:24:38 +0530

    installed styled component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
       <h1 style={{textAlign:'center', color:'blue'}}>Hello World</h1>
+      <h2 className="heading">React APP2</h2>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import classes from './Heading.module.css';
+
+function Button() {
+    return (
+        <div>
+            <h2 style={classes.heading}>React App1</h2>
+        </div>
+    )
+}
+
+export default Button;
